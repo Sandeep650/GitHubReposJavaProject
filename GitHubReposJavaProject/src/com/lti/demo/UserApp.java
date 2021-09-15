0c766c585ea1ebd3e4fe58719322a9ac2d162940
SaiSandeep <sandeepsandy965ne@gmail.com>
2021-09-15 15:19:21 +0530

    first java code in eclipse

--- GitHubReposJavaProject/src/com/lti/demo/UserApp.java
+++ GitHubReposJavaProject/src/com/lti/demo/UserApp.java
@@ -5,5 +5,6 @@ public class UserApp {
 	public static void main(String[] args) {
 		Product p = new Product(101,"Mobile",1000);
 		System.out.println(p);
+		System.out.println("New Line Added");
 	}
 }
